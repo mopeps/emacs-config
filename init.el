f87f810b999c3da2a922cca033265dc5a38a96d4
mopeps <lucagiabb@gmail.com>
2022-02-20 21:57:00 -0300

    Added orgmode config

--- init.el
+++ init.el
@@ -10,9 +10,10 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Iosevka Nerd Font-14" )
+;; (C-q Tab) inserts a tab space
+(add-hook 'ess-mode-hook (lambda () (local-set-key "\t" 'self-insert-command)))
 
-(load-theme 'srcery)
+(set-face-attribute 'default nil :font "Iosevka Nerd Font-14" )
 
 ;; Make Esc quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -182,9 +183,64 @@
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+;; (use-package forge
+;;   :after magit)
 
+(defun bonk/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
 
+;; Org Mode Configuration ------------------------------------------------------
 
+(defun bonk/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Hack Nerd Font" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . bonk/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (bonk/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun bonk/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . bonk/org-mode-visual-fill))
 
 
 
@@ -245,7 +301,7 @@
  '(custom-safe-themes
    '("c5a81a42df109b02a9a68dfe0ed530080372c1a0bbcb374da77ee3a57e1be719" default))
  '(package-selected-packages
-   '(magit counsel-projectile projectile hydra evil-collection evil general srcery-theme helpful counsel ivy-rich which-key rainbow-delimiters use-package ivy doom-themes doom-modeline command-log-mode)))
+   '(org-bullets visual-fill-column magit counsel-projectile projectile hydra evil-collection evil general srcery-theme helpful counsel ivy-rich which-key rainbow-delimiters use-package ivy doom-themes doom-modeline command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
