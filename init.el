726eb7154f4ad95588548a2445e8a939e55413b2
mopeps <lucagiabb@gmail.com>
2022-02-27 13:33:11 -0300

    Cutted startup time for emacs and moved to native emacs

--- init.el
+++ init.el
@@ -20,43 +20,43 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; Use terminal PATH variable
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
+(set-face-attribute 'default nil :font "Iosevka Nerd Font-12" )
+
+;; (C-q Tab) inserts a tab space
+(add-hook 'ess-mode-hook (lambda () (local-set-key "\t" 'self-insert-command)))
+
 ;; Backup and Autosave Directories
-(setq temporary-file-directory "~/.tmp/emacs/")
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-;; Initialize package sources
-(require 'package)
+  (setq temporary-file-directory "~/.tmp/emacs/")
+  (setq backup-directory-alist
+	`((".*" . ,temporary-file-directory)))
+  (setq auto-save-file-name-transforms
+	`((".*" ,temporary-file-directory t)))
+  ;; Initialize package sources
+  (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-
-(setq package-check-signature nil) 
-;; probably not necessary
+  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			   ("org" . "https://orgmode.org/elpa/")
+			   ("elpa" . "https://elpa.gnu.org/packages/")))
 
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(use-package exec-path-from-shell)
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+  (setq package-check-signature nil) 
+  ;; probably not necessary
 
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
-(use-package use-package-ensure-system-package)
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (use-package exec-path-from-shell)
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+
+  ;; Initialize use-package on non-Linux platforms
+  (unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+
+  (require 'use-package)
+  (setq use-package-always-ensure t)
+  (use-package use-package-ensure-system-package)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -66,11 +66,19 @@
   (general-create-definer bonk/leader-keys
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
-    :global-prefix "C-SPC")
+    :global-prefix "C-SPC"))
 
   (bonk/leader-keys
     "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")))
+    "tt" '(counsel-load-theme :which-key "choose theme")
+  ;; Window navigation
+    "H" '(windmove-left :which-key "move to left window")
+    "L" '(windmove-right :which-key "move to right window")
+    "J" '(windmove-down :which-key "move to below window")
+    "K" '(windmove-up :which-key "move to above window")
+  ;; Buffer options
+    "DD" '(kill-this-buffer :which "kills the current buffer")
+    )
 
 (use-package evil
   :init
@@ -95,10 +103,18 @@
   :config
   (evil-collection-init))
 
-(use-package command-log-mode)
+(use-package command-log-mode
+  :commands command-log-mode)
 
 (use-package doom-themes
-  :init (load-theme 'doom-gruvbox t))
+:init (load-theme 'doom-zenburn t))
+(use-package ewal-spacemacs-themes)
+(use-package moe-theme)
+(use-package zenburn-theme)
+(use-package yoshi-theme)
+(use-package sublime-themes)
+(use-package gruvbox-theme)
+(use-package color-theme-sanityinc-tomorrow)
 
 (use-package all-the-icons)
 
@@ -107,9 +123,10 @@
   :custom ((doom-modeline-height 15)))
 
 (use-package which-key
-  :init (which-key-mode)
+  :defer 0
   :diminish which-key-mode
   :config
+  (which-key-mode)
   (setq which-key-idle-delay 1))
 
 (use-package ivy
@@ -142,6 +159,7 @@
   (counsel-mode 1))
 
 (use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
   :custom
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
@@ -151,7 +169,8 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-(use-package hydra)
+(use-package hydra
+  :defer t)
 
 (defhydra hydra-text-scale (:timeout 4)
   "scale text"
@@ -164,16 +183,18 @@
 
 ;; Use ruler in text-mode
 ;;    (add-hook 'text-mode-hook
-       ;;       (function (lambda ()
-	       ;;	   (setq ruler-mode-show-tab-stops t)
-		       ;;   (ruler-mode 1))))
+   ;;       (function (lambda ()
+	   ;;	   (setq ruler-mode-show-tab-stops t)
+		   ;;   (ruler-mode 1))))
 
 (require 'ido)
 (ido-mode 'buffers) ;; only use this line to turn off ido for file names!
 (setq ido-ignore-buffers '("^ " "*Completions*" "*Shell Command Output*"
-			   "*Messages*" "Async Shell Command"))
+		   "*Messages*" "Async Shell Command"))
 
 (use-package org
+  :pin org
+  :commands (org-capture org-agenda)
   :hook (org-mode . bonk/org-mode-setup)
   :config
   (setq org-ellipsis " ▾")
@@ -182,19 +203,21 @@
 (defun bonk/org-font-setup ()
   ;; Replace list hyphen with dot
   (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+			  '(("^ *\\([-]\\) "
+			 (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
   ;; Set faces for heading levels
   (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Hack Nerd Font" :weight 'regular :height (cdr face)))
+		  (org-level-2 . 1.1)
+		  (org-level-3 . 1.05)
+		  (org-level-4 . 1.0)
+		  (org-level-5 . 1.1)
+		  (org-level-6 . 1.1)
+		  (org-level-7 . 1.1)
+		  (org-level-8 . 1.1)))
+(set-face-attribute (car face) nil :font "Hack Nerd Font"
+			:weight 'regular
+			:height (cdr face)))
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
   (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
@@ -213,10 +236,11 @@
 
 (defun bonk/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
+		visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
+  :after org
   :hook (org-mode . bonk/org-mode-visual-fill))
 
 ;; Automatically tangle our Emacs.org config file when we save it
@@ -228,12 +252,17 @@
       (org-babel-tangle)))
   (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'bonk/org-babel-tangle-config))))
 
-(org-babel-do-load-languages
-  'org-babel-load-languages
-  '((emacs-lisp . t)
-    (python . t)))
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+    'org-babel-load-languages
+    '((emacs-lisp . t)
+      (python . t)))
 
-(push '("conf-unix" . conf-unix) org-src-lang-modes)
+  (push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4) ; I want tabs to be four spaces wide
+;; Indentation levels for each lang
 
 ;; Increase for better lsp-mode performance; see
 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
@@ -247,7 +276,6 @@
   (lsp-headerline-breadcrumb-mode))
 
 (use-package lsp-mode
-  :ensure t
   :commands (lsp lsp-deferred)
   :hook (lsp-mode . bonk/lsp-mode-setup)
   :init
@@ -255,7 +283,7 @@
   :config
 
   (add-hook 'typescript-mode-hook 'lsp)
-  (add-hook 'js2-mode-hook 'lsp)
+  (add-hook 'rjsx-mode-hook 'lsp)
   (add-hook 'php-mode 'lsp)
   (add-hook 'css-mode 'lsp)
   (add-hook 'web-mode 'lsp)
@@ -279,7 +307,7 @@
 					(haskell-mode . "haskell")
 					(php-mode . "php")
 					(json-mode . "json")
-					(js2-mode . "javascript")
+					(rjsx-mode . "javascript")
 					(typescript-mode . "typescript")
 					))
 
@@ -296,51 +324,54 @@
 (use-package lsp-treemacs
   :after lsp)
 
-(use-package lsp-ivy)
+(use-package lsp-ivy
+  :after lsp)
 
 
 
 (use-package yasnippet                  ; Snippets
-  :ensure t
+  :after lsp
   :config
   (setq
    yas-verbosity 1                      ; No need to be so verbose
    yas-wrap-around-region t)
 
   (with-eval-after-load 'yasnippet
-    (setq yas-snippt-dirs '(yasnippet-snippets-dir)))
+	(setq yas-snippt-dirs '(yasnippet-snippets-dir)))
 
   (yas-reload-all)
   (yas-global-mode))
 
 (use-package yasnippet-snippets         ; Collection of snippets
-  :ensure t)
+  :after yasnippet)
 
-(use-package flycheck)
+(use-package flycheck
+  :after lsp)
 (setq flycheck-disabled-checkers '(ruby ruby-reek ruby-rubocop ruby-rubylint yaml-ruby))
 
 (use-package typescript-mode
   :mode "\\.ts\\'"
-  :hook (typescript-mode . lsp-deferred)
-  :config
-  (setq typescript-indent-level 2))
+  :hook (typescript-mode . lsp-deferred))
 
-(use-package inf-ruby)
+(use-package inf-ruby
+  :after lsp-mode)
 
 (use-package ruby-mode
-  :ensure nil
   :after lsp-mode
   :mode "\\.rb\\'"
   :hook ((ruby-mode . lsp-deferred)
-	 (ruby-mode . amk-lsp-format-on-save))
+		 (ruby-mode . amk-lsp-format-on-save))
   :config
-  (setq ruby-indent-level 2)
-  (setq ruby-indent-tabs-mode t))
+  (setq ruby-indent-tabs-mode t)
+  (setq ruby-indent-level tab-width))
+
 (use-package enh-ruby-mode
+  :after lsp-mode
   :config
   (setq enh-ruby-indent-tabs-mode t))
 
-(use-package robe)
+(use-package robe
+  :after lsp)
 (add-hook 'ruby-mode-hook 'robe-mode)
 (eval-after-load 'company
   '(push 'company-robe company-backends))
@@ -348,79 +379,73 @@
 (require 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
-(use-package rspec-mode)
+(use-package rspec-mode
+  :after (lsp ruby-mode))
 (eval-after-load 'rspec-mode
- '(rspec-install-snippets))
+  '(rspec-install-snippets))
 
-(use-package go-mode :ensure t)
-      (add-hook 'go-mode-hook (lambda ()
-				(setq tab-width 4)))
-      (require 'lsp-mode)
-      (add-hook 'go-mode-hook #'lsp)
-      (add-hook 'before-save-hook 'gofmt-before-save)
-      (defun lsp-go-install-save-hooks ()
+(use-package go-mode
+  :mode "\\.go'\\"
+  :after lsp-mode
+  :config
+  (add-hook 'go-mode-hook (lambda ()
+							(setq tab-width 4)))
+  (add-hook 'go-mode-hook #'lsp)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (defun lsp-go-install-save-hooks ()
 	(add-hook 'before-save-hook 'lsp-format-buffer t t)
 	(add-hook 'before-save-hook 'lsp-organize-imports t t))
-      (add-hook 'go-mode-hook 'lsp-go-install-save-hooks)
-;; configure gopls
-      (lsp-register-custom-settings
-       '(("gopls.completeUnimported" t t)
+  (add-hook 'go-mode-hook 'lsp-go-install-save-hooks)
+  ;; configure gopls
+  (lsp-register-custom-settings
+   '(("gopls.completeUnimported" t t)
 	 ("gopls.staticcheck" t t)))
-      ;; Start LSP Mode and YASnippet mode
-      (add-hook 'go-mode-hook 'lsp-deferred)
-      (add-hook 'go-mode-hook 'yas-minor-mode)
+  ;; Start LSP Mode and YASnippet mode
+  (add-hook 'go-mode-hook 'lsp-deferred)
+  (add-hook 'go-mode-hook 'yas-minor-mode))
 
-(use-package add-node-modules-path :ensure t)
-(use-package web-mode :ensure t)
-(use-package js2-mode :ensure t)
-(use-package rjsx-mode :ensure t)
-(add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
-(with-eval-after-load 'js2-mode
-  (defun bonk-js-mode-defaults ()
-    ;; electric-layout-mode doesn't play nice with smartparens
-    (setq-local electric-layout-rules '((?\; . after)))
-    (setq mode-name "JS2")
-    (js2-imenu-extras-mode +1)
-    (subword-mode +1))
-
-  (setq bonk-js-mode-hook 'bonk-js-mode-defaults)
-
-  (add-hook 'js2-mode-hook (lambda () (run-hooks 'bonk-js-mode-hook))))
-;; disable default jsonlint
-(setq-default flycheck-disabled-checkers
-	      (append flycheck-disabled-checkers
-		      '(javascript-jshint json-jsonlist)))
-;; using a global eslint
-(require 'flycheck)
-
-;; Enable eslint checker for web-mode
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-;; Enable flycheck globally
-(add-hook 'after-init-hook #'global-flycheck-mode)
-;; add node modules path
-(add-hook 'flycheck-mode-hook 'add-node-modules-path)
-
-(use-package tide
+(use-package rjsx-mode
   :ensure t
-  :after (typescript-mode company flycheck)
-  :hook ((typescript-mode . tide-setup)
-	 (typescript-mode . tide-hl-identifier-mode)
-	 (before-save . tide-format-before-save)))
+  :mode "\\.js\\'"
+  :config
+  (setq indent-tabs-mode t)
+  (setq js2-basic-offset 4)
+  (defvaralias 'js-indent-level 'tab-width)
+  (defvaralias 'js-indent-level 'tab-width)
+  (defvaralias 'rjsx-indent-level 'tab-width)
+  (defvaralias 'rjsx-mode-markup-indent-offset 'tab-width)
+  (defvaralias 'rjsx-mode-code-indent-offset 'tab-width)
+  (defvaralias 'web-mode-css-indent-offset 'tab-width)
+  (defvaralias 'js2-indent-level 'tab-width))
+
+(defun setup-tide-mode()
+  "Setup function for tide."
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
+
+  (use-package tide
+	:after (rjsx-mode company flycheck)
+	:hook (rjsx-mode . setup-tide-mode))
+
+(use-package prettier-js
+  :after (rjsx-mode)
+  :hook (rjsx-mode . setup-tide-mode))
 
 ;; yaml-mode doesn't derive from prog-mode, but we can at least enable
 ;; whitespace-mode and apply cleanup.
-(use-package yaml-mode)
-(add-hook 'yaml-mode-hook 'whitespace-mode)
-(add-hook 'yaml-mode-hook 'subword-mode)
+(use-package yaml-mode
+  :after lsp-mode
+  :config
+  (add-hook 'yaml-mode-hook 'whitespace-mode)
+  (add-hook 'yaml-mode-hook 'subword-mode))
 
 (use-package company
   :after lsp-mode
   :hook (lsp-mode . company-mode)
   :bind (:map company-active-map
-	      ("<tab>" . company-complete-selection))
+	  ("<tab>" . company-complete-selection))
   (:map lsp-mode-map
 	("<tab>" . company-indent-or-complete-common))
   :custom
@@ -436,12 +461,12 @@
 
 
 (use-package company-inf-ruby
-  :after (company ruby-mode)
-  :config (add-to-list 'company-backends 'company-inf-ruby))
+:after (company ruby-mode)
+:config (add-to-list 'company-backends 'company-inf-ruby))
 
 (use-package ac-js2
-  :after (company tide js2-mode web-mode)
-  :config (add-to-list 'company-backends 'ac-js2))
+:after (company tide js2-mode web-mode)
+:config (add-to-list 'company-backends 'ac-js2))
 
 ;; HTML company backend
        (use-package company-web
@@ -451,9 +476,9 @@
 
 ;; Add `company-elisp' backend for elisp.
 (add-hook 'emacs-lisp-mode-hook
-	  '(lambda ()
-	     (require 'company-elisp)
-	     (push 'company-elisp company-backends)))
+		  '(lambda ()
+			 (require 'company-elisp)
+			 (push 'company-elisp company-backends)))
 
 (use-package projectile
   :diminish projectile-mode
@@ -483,12 +508,14 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package term
+  :commands term
   :config
   (setq explicit-shell-file-name "zsh") ;; You can change this to bash, fish, etc
   ;;(setq explicit-zsh-args '()) ;; Use it to set especific shell args
   (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
 
 (use-package eterm-256color
+  :after term
   :hook (term-mode . eterm-256color-mode))
 
 (use-package vterm
@@ -502,38 +529,44 @@
   (setq vterm-kill-buffer-on-exit t)
   (setq vterm-timer-delay 0.01))
 
+(bonk/leader-keys
+  "vt" '(vterm-other-window :which-key "vterm in new window")
+  "vb" '(vterm :which-key "open new buffer for vterm"))
+
 (setq backup-directory-alist            '((".*" . "~/.Trash")))
 
 (use-package dired
-  :ensure nil
-  :commands (dired dired-jump)
-  :bind (("C-x C-j" . dired-jump))
-  :custom ((setq insert-directory-program "gls" dired-use-ls-dired t)
-	   (setq dired-listing-switches "-al --group-directories-first"))
-  :config
-  (evil-collection-define-key 'normal 'dired-mode-map
-    "h" 'dired-single-up-directory
-    "l" 'dired-single-buffer))
+:ensure nil
+:commands (dired dired-jump)
+:bind (("C-x C-j" . dired-jump))
+:custom ((setq insert-directory-program "gls" dired-use-ls-dired t)
+	 (setq dired-listing-switches "-al --group-directories-first"))
+:config
+(evil-collection-define-key 'normal 'dired-mode-map
+  "h" 'dired-single-up-directory
+  "l" 'dired-single-buffer))
 
-(use-package dired-single)
+  (use-package dired-single)
 
-(use-package all-the-icons-dired
-  :hook (dired-mode . all-the-icons-dired-mode))
+  (use-package all-the-icons-dired
+:hook (dired-mode . all-the-icons-dired-mode))
 
-(use-package dired-open
-  :config
-  ;; Doesn't work as expected!
-  ;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
-  (setq dired-open-extensions '(("png" . "feh")
-				("mkv" . "mpv"))))
+  (use-package dired-open
+:config
+;; Doesn't work as expected!
+;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
+(setq dired-open-extensions '(("png" . "feh")
+				  ("mkv" . "mpv"))))
 
-(use-package dired-hide-dotfiles
-  :hook (dired-mode . dired-hide-dotfiles-mode)
-  :config
-  (evil-collection-define-key 'normal 'dired-mode-map
-    "H" 'dired-hide-dotfiles-mode))
+  (use-package dired-hide-dotfiles
+:hook (dired-mode . dired-hide-dotfiles-mode)
+:config
+(evil-collection-define-key 'normal 'dired-mode-map
+  "H" 'dired-hide-dotfiles-mode))
 
-(require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(with-eval-after-load 'org
+	(require 'org-tempo)
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("rb" . "src ruby")))
