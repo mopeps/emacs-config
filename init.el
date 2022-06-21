81161ae69819a5d7bcc95c4ca91a247a72194601
mopeps <lucagiabb@gmail.com>
2022-06-21 15:44:44 -0300

    set guile as scheme program

--- init.el
+++ init.el
@@ -642,19 +642,21 @@
   :mode ("\\(\\.v?v\\|\\.vsh\\)$'" . 'v-mode))
 
 (add-hook 'emacs-lisp-mode-hook 'yas-minor-mode)
-(add-hook 'emacs-lisp-mode-hook 'company-mode)
+  (add-hook 'emacs-lisp-mode-hook 'company-mode)
 
-(use-package slime
-  :ensure t
-  :config
-  (setq inferior-lisp-program "/usr/bin/sbcl")
-  (setq slime-contribs '(slime-fancy)))
+  (use-package slime
+	:ensure t
+	:config
+	(setq inferior-lisp-program "/usr/bin/sbcl")
+	(setq slime-contribs '(slime-fancy)))
 
-(use-package slime-company
-  :ensure t
-  :init
-	(require 'company)
-	(slime-setup '(slime-fancy slime-company)))
+  (use-package slime-company
+	:ensure t
+	:init
+	  (require 'company)
+	  (slime-setup '(slime-fancy slime-company)))
+(use-package geiser)
+  (set-variable (quote scheme-program-name) "guile")
 
 (use-package geiser-guile
   :ensure t)
